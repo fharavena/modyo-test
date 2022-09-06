5eb9e20076f1fad203ae3194fd5343d100eb636b
fharavena <fharavena@gmail.com>
2022-09-05 23:10:59 -0300

    adding cross origin *

--- src/main/java/modyo/com/pokedex/controllers/PokemonController.java
+++ src/main/java/modyo/com/pokedex/controllers/PokemonController.java
@@ -16,6 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/pokemon")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PokemonController {
     @Autowired
     private PokeServiceImpl pokeServiceImpl;
